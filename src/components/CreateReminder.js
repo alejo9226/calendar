5c7573ac12431af58690faa5bee0aa700f27be9b
Alejandro Alfaro <alejo9226@gmail.com>
2021-02-22 10:12:24 -0500

    fix: create-reminder inline styles

--- src/components/CreateReminder.js
+++ src/components/CreateReminder.js
@@ -37,80 +37,141 @@ export default function CreateReminder ({ dateToAdd, open, setOpen, setOpenDialo
   }
 
   return (
-    <form onSubmit={addReminder} data-testid='create-reminder'>
-      <label htmlFor='description'>
-        Description
-      </label>
-      <input 
-        type="text"
-        id="description"
-        name="description"
-        value={description}
-        onChange={handleInputChange} 
-        required
-      />
-      <label htmlFor='city'>
-        City
-      </label>
-      <input 
-        type="text"
-        id="city"
-        name="city"
-        value={city}
-        onChange={handleInputChange} 
-        required
-      />
-      <label htmlFor='color'>Color</label>
-      <select
-        type='text'
-        name='color'
-        id='color'
-        value={color}
-        onChange={handleInputChange}
-        required
-      >
-        <option>Choose color</option>
-        {!!colors &&
-          colors.length > 0 &&
-          colors.map((color) => {
-            return (
-              <option 
-                value={color.name} 
-                key={color.name}
-                style={{ backgroundColor: color.color }}
+    <div className="view-modal">
+      <div>
+        <span className="modal__title">Reminder Details</span>
+      </div>
+      <div>
+        <form onSubmit={addReminder} data-testid='create-reminder' style={{
+          display: 'flex',
+          flexDirection: 'column'
+        }}>
+          <div
+            style={{
+              display: 'flex',
+              marginTop: '20px',
+              padding: '0 5px'
+            }}
+          >
+            <div
+              style={{
+                display: 'flex',
+                marginRight: '10px',
+                
+              }}
+            >
+              <label htmlFor='city' className="key">
+                City
+              </label>
+              <input 
+                type="text"
+                id="city"
+                name="city"
+                value={city}
+                onChange={handleInputChange} 
+                required
+              />
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                marginRight: '10px',
+                
+              }}
+            >
+              <label htmlFor='color' className="key">Color</label>
+              <select
+                type='text'
+                name='color'
+                id='color'
+                value={color}
+                onChange={handleInputChange}
+                required
               >
-                {color.name}
-              </option>
-            )
-          })}
-      </select>
-      <label htmlFor='time'>Time</label>
-      <select
-        type='text'
-        name='time'
-        id='time'
-        value={time}
-        onChange={handleInputChange}
-        required
-      >
-        <option>Choose time</option>
-        {!!hours &&
-          hours.length > 0 &&
-          hours.map((hour) => {
-            return (
-              <option 
-                value={hour} 
-                key={hour}
+                <option>Choose color</option>
+                {!!colors &&
+                  colors.length > 0 &&
+                  colors.map((color) => {
+                    return (
+                      <option 
+                        value={color.name} 
+                        key={color.name}
+                        style={{ backgroundColor: color.color }}
+                      >
+                        {color.name}
+                      </option>
+                    )
+                  })}
+              </select>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+              }}
+            >
+              <label htmlFor='time' className="key">Time</label>
+              <select
+                type='text'
+                name='time'
+                id='time'
+                value={time}
+                onChange={handleInputChange}
+                required
               >
-                {hour}
-              </option>
-            )
-          })}
-      </select>
-      {errors && <p className="contentInput__error">{errors}</p>}
-      <button type="submit">
-        Add reminder
-      </button>
-    </form>
+                <option>Choose time</option>
+                {!!hours &&
+                  hours.length > 0 &&
+                  hours.map((hour) => {
+                    return (
+                      <option 
+                        value={hour} 
+                        key={hour}
+                      >
+                        {hour}
+                      </option>
+                    )
+                  })}
+              </select>
+            </div>
+          </div>
+          
+          <div
+            style={{
+              display: 'flex',
+              width: '100%',
+              marginTop: '20px',
+              marginBottom: '15px',
+              padding: '0 5px'
+            }}
+          >
+            <div
+             style={{
+               display: 'flex',
+               flex: 1 
+             }}
+            >
+              <label htmlFor='description' className="key">
+                Description
+              </label>
+              <input 
+                style={{
+                  width: '100%'
+                }}
+                type="text"
+                id="description"
+                name="description"
+                value={description}
+                onChange={handleInputChange} 
+                required
+              />
+            </div>
+          </div>
+          {errors && <p className="contentInput__error">{errors}</p>}
+          <button type="submit" className="create__button">
+            Add reminder
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
